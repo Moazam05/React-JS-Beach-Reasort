0def17826c44c3c636dbe594ad28c91f9369d24e
Moazam05 <salmanmoazam08@gmail.com>
2020-04-01 00:31:56 +0500

    Hero Component

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,7 +1,9 @@
 import React from "react";
 
+import Hero from "../components/Hero";
+
 const Rooms = () => {
-  return <div>Hello From Rooms Page</div>;
+  return <Hero hero="roomsHero" />;
 };
 
 export default Rooms;
