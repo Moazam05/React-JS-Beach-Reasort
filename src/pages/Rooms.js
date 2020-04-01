d2665a193ff0a8d4f8606bc987a0721530b296de
Moazam05 <salmanmoazam08@gmail.com>
2020-04-01 16:17:05 +0500

    Banner Component

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+
+import { Link } from "react-router-dom";
 
 const Rooms = () => {
-  return <Hero hero="roomsHero" />;
+  return (1
+    <Hero hero="roomsHero">
+      <Banner title="our rooms">
+        <Link to="/" className="btn-primary">
+          retun home
+        </Link>
+      </Banner>
+    </Hero>
+  );
 };
 
 export default Rooms;
