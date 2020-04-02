7e7593fbda73a7f3c06cbf0b58a0e08cfd74d83a
Moazam05 <salmanmoazam08@gmail.com>
2020-04-02 14:53:40 +0500

    Room List

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,19 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
-
-import { Link } from "react-router-dom";
+import RoomsContainer from "../components/RoomsContainer";
 
 const Rooms = () => {
   return (
-    <Hero hero="roomsHero">
-      <Banner title="our rooms">
-        <Link to="/" className="btn-primary">
-          retun home
-        </Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero hero="roomsHero">
+        <Banner title="our rooms">
+          <Link to="/" className="btn-primary">
+            retun home
+          </Link>
+        </Banner>
+      </Hero>
+      <RoomsContainer />
+    </>
   );
 };
 
